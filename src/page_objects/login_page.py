1aaad63a3f14e340347b1d99408fd5d99f8a7daa
Bar Perach <bar@aironworks.com>
2024-04-23 21:50:18 +0900

    fix login locator

--- src/page_objects/login_page.py
+++ src/page_objects/login_page.py
@@ -1,4 +1,5 @@
 import allure
+import re
 from playwright.sync_api import Page
 
 from src.models.user_model import UserModel
@@ -8,11 +9,12 @@ from src.page_objects.base_page import BasePage
 class SignInPage(BasePage):
     def __init__(self, page: Page):
         super().__init__(page)
-        page.set_default_timeout(120 * 1000)
         self.button_sign_in_email = page.get_by_text("Sign in with Email")
         self.input_email = page.locator('[name="email"]')
         self.input_password = page.locator('[name="password"]')
-        self.button_sign_in = page.locator('[id="mui-3"]')
+        self.button_sign_in = page.get_by_role(
+            "button", name=re.compile("sign in$", re.IGNORECASE)
+        )
 
     @allure.step("SignInPage: open page")
     def navigate(self):
