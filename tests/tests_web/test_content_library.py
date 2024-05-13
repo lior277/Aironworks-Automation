28b347684989f7ed727b18478ac6c83f2a5b003a
Bar Perach <bar@aironworks.com>
2024-05-13 16:32:04 +0900

    add forgotten mark

--- tests/tests_web/test_content_library.py
+++ tests/tests_web/test_content_library.py
@@ -4,6 +4,7 @@ from src.models.factories.user_model_factory import UserModelFactory
 
 
 @pytest.mark.parametrize("user", [UserModelFactory.aw_admin()])
+@pytest.mark.test_id("C31520")
 def test_filter_company_by_visiblity(user, sign_in_page):
     sign_in_page.navigate(admin=user.is_admin)
     sign_in_page.submit_sign_in_form(user)
