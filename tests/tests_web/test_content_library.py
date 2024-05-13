fe5616d8e4eee4b9d2215f80deddd8ee2b94bc65
Bar Perach <bar@aironworks.com>
2024-05-13 15:56:54 +0900

    tests: implement filter test for visibility

---
+++ tests/tests_web/test_content_library.py
@@ -0,0 +1,14 @@
+import pytest
+from playwright.sync_api import expect
+from src.models.factories.user_model_factory import UserModelFactory
+
+
+@pytest.mark.parametrize("user", [UserModelFactory.aw_admin()])
+def test_filter_company_by_visiblity(user, sign_in_page):
+    sign_in_page.navigate(admin=user.is_admin)
+    sign_in_page.submit_sign_in_form(user)
+
+    content_library_page = sign_in_page.navigation_bar.navigate_content_library()
+    content_library_page.set_visibility_filter("Test Company 75000")
+
+    expect(content_library_page.cards).to_have_count(1)
