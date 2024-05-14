74b32e6f55f2e9c0909a0f96dd5563db5d73d192
Bar Perach <bar@aironworks.com>
2024-05-14 14:24:00 +0900

    2 more content library tests

--- tests/tests_web/test_content_library.py
+++ tests/tests_web/test_content_library.py
@@ -1,4 +1,5 @@
 import pytest
+import re
 from playwright.sync_api import expect
 from src.models.factories.user_model_factory import UserModelFactory
 
@@ -39,3 +40,30 @@ def test_filter_company_by_name(user, sign_in_page):
 
     expect(content_library_page.cards).to_have_count(1)
     expect(content_library_page.cards.first).to_contain_text("Authentication 101")
+
+
+@pytest.mark.parametrize(
+    "user",
+    [
+        pytest.param(
+            UserModelFactory.aw_admin(),
+            id="AW Admin",
+            marks=pytest.mark.test_id("C31516"),
+        ),
+        pytest.param(
+            UserModelFactory.customer_admin(),
+            id="Customer Admin",
+            marks=pytest.mark.test_id("C31517"),
+        ),
+    ],
+)
+def test_full_state_and_empty_state(user, sign_in_page):
+    sign_in_page.navigate(admin=user.is_admin)
+    sign_in_page.submit_sign_in_form(user)
+
+    content_library_page = sign_in_page.navigation_bar.navigate_content_library()
+    content_library_page.set_name_filter("qwepqwjoeinqwourbqiouwboasnodnqwoienoquwbeq")
+
+    expect(content_library_page.cards).to_have_count(0)
+    expect(content_library_page.empty_state).to_be_visible()
+    expect(content_library_page.page.get_by_text(re.compile("Oops!.*"))).to_be_visible()
