1c648b792d64431ee905df89d5275040314da7f5
Bar Perach <bar@aironworks.com>
2024-05-14 14:12:40 +0900

    test: content library filter test

--- tests/tests_web/test_content_library.py
+++ tests/tests_web/test_content_library.py
@@ -13,3 +13,29 @@ def test_filter_company_by_visiblity(user, sign_in_page):
     content_library_page.set_visibility_filter("Test Company 75000")
 
     expect(content_library_page.cards).to_have_count(1)
+
+
+@pytest.mark.parametrize(
+    "user",
+    [
+        pytest.param(
+            UserModelFactory.aw_admin(),
+            id="AW Admin",
+            marks=pytest.mark.test_id("C31518"),
+        ),
+        pytest.param(
+            UserModelFactory.customer_admin(),
+            id="Customer Admin",
+            marks=pytest.mark.test_id("C31519"),
+        ),
+    ],
+)
+def test_filter_company_by_name(user, sign_in_page):
+    sign_in_page.navigate(admin=user.is_admin)
+    sign_in_page.submit_sign_in_form(user)
+
+    content_library_page = sign_in_page.navigation_bar.navigate_content_library()
+    content_library_page.set_name_filter("Authentication 101")
+
+    expect(content_library_page.cards).to_have_count(1)
+    expect(content_library_page.cards.first).to_contain_text("Authentication 101")
