542da6d88200ee5823aeec80528385bfa6171c7a
Vitalii Yalantis <vitalii@aironworks.com>
2024-05-28 09:18:22 +0000

    fixed education content performance test

--- tests/tests_perf/education_content_perf.py
+++ tests/tests_perf/education_content_perf.py
@@ -5,11 +5,18 @@ from gevent.event import Event
 from locust import FastHttpUser, task, SequentialTaskSet, events
 from locust.exception import StopUser
 
-from src.configs.config_loader import AppConfigs
-
 logger = logging.getLogger("locust")
 
 all_users_spawned = Event()
+
+
+def load_csv(filename):
+    with open(filename, mode='r') as file:
+        reader = csv.DictReader(file)
+        return list(reader)
+
+
+csv_data = load_csv("perf_education_campaign.csv")  # Change the file path for local execution
 
 
 @events.init.add_listener
@@ -20,59 +27,44 @@ def on_locust_init(environment, **kwargs):
         all_users_spawned.set()  # Signal that all users have been spawned
 
 
-def load_csv(filename):
-    with open(filename, mode='r') as file:
-        reader = csv.DictReader(file)
-        return list(reader)
-
-
-csv_data = load_csv("tests/resources/perf_education_campaign.csv")
-
-
 class MySequentialTaskSet(SequentialTaskSet):
     def on_start(self):
-        self.user_data = csv_data[self.user.index % len(csv_data)]
         logger.info("wait self")
         self.wait()
 
     @task
     def get_education_assignment(self):
         user_index = self.user.index
-        logger.info(f"user_index = {user_index}")
-        logger.info(f"user_data = {self.user_data}")
-        email = self.user_data["email"]
-        assignment_id = self.user_data["id"]
-        token = self.user_data["token"]
+        logger.info(f"{user_index=}")
+        data = csv_data[user_index]
+        logger.info(f"{data=}")
+        data_values = list(data.values())
+        logger.info(f"{data_values=}")
 
-        response = self.client.get(f"/api/education/assignment/{assignment_id}",
-                                   params={"email": email, "token": token},
-                                   name="/api/education/assignment/{education_id}")
-
-        logger.info(response.json())
+        assignment_id = data_values[1]
+        url = f"/api/education/assignment/{assignment_id}"
+        param = {"email": data_values[0], "token": data_values[4]}
+        full_url = f"{self.client.base_url}{url}"
+        with self.client.get(url, params=param, catch_response=True,
+                             name="/api/education/assignment/{education_id}") as response:
+            if response.status_code != 200:
+                response.failure(f"{param=},{url=},{full_url=},{response.text=},{response.status_code=}")
         self.complete_task()
 
     def complete_task(self):
-        self.user.task_index += 1
-        if self.user.task_index >= len(self.tasks):
-            logger.info(f"stop user {self.user.index}")
-            raise StopUser()
-
-    def stop(self):
-        # Check if all tasks have been completed
-        if self.user.task_index >= len(self.tasks):
-            self.user.environment.runner.quit()
+        logger.info(f"stop user {self.user.index}")
+        raise StopUser()
 
 
 class CustomHttpUser(FastHttpUser):
     connection_timeout = 60.0
     insecure = False
     network_timeout = 60.0
-    host = AppConfigs.BASE_URL
+    host = "https://staging.app.aironworks.com"
     tasks = [MySequentialTaskSet]
     user_index = 0
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.task_index = 0
         self.index = CustomHttpUser.user_index
         CustomHttpUser.user_index += 1
