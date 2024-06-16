752e2242a6a40fe628c8c5860add0a38c035c9b3
Bar Perach <bar@aironworks.com>
2024-06-16 14:31:55 +0900

    fix: fix functional perf test

--- tests/test_perf_functional.py
+++ tests/test_perf_functional.py
@@ -6,15 +6,22 @@ from src.models.company.employee_delete_model import EmployeeDeleteModel
 from src.models.company.employee_list_ids_model import EmployeeListIdsModel
 from src.models.company.employee_update_model import EmployeeUpdateModel
 from src.models.factories.company.employee_model_factory import EmployeeModelFactory
-from src.models.factories.education_campaign.education_campaign_model_factory import EducationCampaignModelFactory
-from src.models.general_models import BasicModel
+from src.models.factories.education_campaign.education_campaign_model_factory import (
+    EducationCampaignModelFactory,
+)
+from src.models.general_models import BasicModel, LongRunningOperation
+from src.utils.waiter import wait_for_lro
 from src.models.mait_trap_model import MailTrapModelFactory
 from src.utils.list import divide_list_into_chunks
+from playwright.sync_api import expect
 
 
 @pytest.mark.parametrize("employees_count", [1])
 @pytest.mark.delivered_emails
-def test_education_campaign_emails_delivered(api_request_context_customer_admin, mailtrap, employees_count: int):
+@pytest.mark.timeout(60 * 100)
+def test_education_campaign_emails_delivered(
+    api_request_context_customer_admin, mailtrap, employees_count: int
+):
     """
 
     :param api_request_context_customer_admin:
@@ -29,43 +36,76 @@ def test_education_campaign_emails_delivered(api_request_context_customer_admin,
     mailtrap.clean_inboxes(mail_trap_inboxes)
 
     # remove all employees
-    response = CompanyService.get_employee_ids(api_request_context_customer_admin,
-                                               EmployeeListIdsModel(employee_role=True, admin_role=False, filters=None))
-    if response.json()['items']:
-        CompanyService.update_employees(api_request_context_customer_admin,
-                                        employees=EmployeeUpdateModel(employee_role=False,
-                                                                      ids=list(response.json()['items'])))
-    response = CompanyService.get_employee_ids(api_request_context_customer_admin,
-                                               EmployeeListIdsModel(employee_role=False, admin_role=False,
-                                                                    filters=None))
+    response = CompanyService.get_employee_ids(
+        api_request_context_customer_admin,
+        EmployeeListIdsModel(employee_role=True, admin_role=False, filters=None),
+    )
+    if response.json()["items"]:
+        CompanyService.update_employees(
+            api_request_context_customer_admin,
+            employees=EmployeeUpdateModel(
+                employee_role=False, ids=list(response.json()["items"])
+            ),
+        )
+    response = CompanyService.get_employee_ids(
+        api_request_context_customer_admin,
+        EmployeeListIdsModel(employee_role=False, admin_role=False, filters=None),
+    )
     if response.json()["items"]:
         divided_list = divide_list_into_chunks(response.json()["items"], 2000)
         for chunk in divided_list:
-            CompanyService.delete_employees(api_request_context_customer_admin,
-                                            employees=EmployeeDeleteModel(ids=chunk))
+            CompanyService.delete_employees(
+                api_request_context_customer_admin,
+                employees=EmployeeDeleteModel(ids=chunk),
+            )
     # create {employees_count} employees for each inbox
     for mail_trap_inbox in mail_trap_inboxes:
-        employees = EmployeeModelFactory.get_random_employees(employees_count, mailtrap_inbox=mail_trap_inbox.email)
-        response = CompanyService.create_employees(api_request_context_customer_admin, employees, overwrite=False)
-        response_body = BasicModel.from_dict(response.json())
-        assert response_body.data.success and response.ok, \
-            f"Failed to upload file with employee. Response => {response_body}"
+        employees = EmployeeModelFactory.get_random_employees(
+            employees_count, mailtrap_inbox=mail_trap_inbox.email
+        )
+        response = CompanyService.create_employees(
+            api_request_context_customer_admin, employees, overwrite=False
+        )
+        expect(response).to_be_ok()
+        response_body = LongRunningOperation.from_dict(response.json())
+        assert response_body.status == "CREATED"
+        id = response_body.id
+        result = wait_for_lro(
+            lambda: CompanyService.create_employees_status(
+                api_request_context_customer_admin, id
+            ),
+            timeout=60 * 2,
+        )
+        expect(result).to_be_ok()
+        response_body = LongRunningOperation.from_dict(result.json())
+
+        assert (
+            response_body.status == "DONE"
+        ), f"Failed to upload file with employee. Response => {response_body}"
         employees_list.extend(employees)
 
-    response = CompanyService.get_employee_ids(api_request_context_customer_admin,
-                                               EmployeeListIdsModel(employee_role=True, filters=None))
+    response = CompanyService.get_employee_ids(
+        api_request_context_customer_admin,
+        EmployeeListIdsModel(employee_role=True, filters=None),
+    )
     employee_ids = response.json()
-    assert len(employee_ids["items"]) >= len(employees_list), \
-        f"Expected employees => {len(employees_list)}\nActual employees => {len(employee_ids["items"])}"
-    education_campaign = EducationCampaignModelFactory.get_education_campaign(employee_ids["items"])
+    assert (
+        len(employee_ids["items"]) >= len(employees_list)
+    ), f"Expected employees => {len(employees_list)}\nActual employees => {len(employee_ids["items"])}"
+    education_campaign = EducationCampaignModelFactory.get_education_campaign(
+        employee_ids["items"]
+    )
 
     # start education campaign
-    response = EducationService.start_campaign(api_request_context_customer_admin, education_campaign)
-    assert response.ok
+    response = EducationService.start_campaign(
+        api_request_context_customer_admin, education_campaign
+    )
+    expect(response).to_be_ok()
     employees_email_list = [employee.email for employee in employees_list]
 
     # Wait for all emails
-    emails_left = mailtrap.wait_for_all_mail_in_diff_inboxes(employees_email_list, list_mail_traps=mail_trap_inboxes,
-                                                             remove_messages=True)
+    emails_left = mailtrap.wait_for_all_mail_in_diff_inboxes(
+        employees_email_list, list_mail_traps=mail_trap_inboxes, remove_messages=True
+    )
 
     assert not emails_left, f"{len(emails_left)=} {emails_left=}"
