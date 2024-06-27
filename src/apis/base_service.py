3a5c33fd067d01109232e23854e76150f3ea4553
Bar Perach <bar@aironworks.com>
2024-06-27 15:51:17 +0900

    fix: deactivate customer

--- src/apis/base_service.py
+++ src/apis/base_service.py
@@ -5,8 +5,11 @@ from playwright.sync_api import APIRequestContext, APIResponse
 
 
 def allure_attach(response: APIResponse):
-    allure.attach(body=json.dumps(response.json(), indent=4), attachment_type=allure.attachment_type.JSON,
-                  name="response")
+    allure.attach(
+        body=json.dumps(response.json(), indent=4),
+        attachment_type=allure.attachment_type.JSON,
+        name="response",
+    )
 
 
 class BaseService:
@@ -15,7 +18,9 @@ class BaseService:
         self._request.on("response", allure_attach)
 
     def _post(self, url, data=None, params=None, timeout=None) -> APIResponse:
-        response = self._request.post(url=url, data=data, params=params, timeout=timeout)
+        response = self._request.post(
+            url=url, data=data, params=params, timeout=timeout
+        )
         allure_attach(response)
         return response
 
