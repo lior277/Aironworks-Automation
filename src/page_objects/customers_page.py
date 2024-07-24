bdd59f19e52baf79546a7ad5e6da2e0eaf079524
vitalii.misha <vitalii.misha@yalantis.net>
2024-07-24 16:05:05 +0300

    Fixed customer management test cases

--- src/page_objects/customers_page.py
+++ src/page_objects/customers_page.py
@@ -1,34 +1,23 @@
 import re
-import allure
 from typing import Literal
+
+import allure
 from playwright.sync_api import Page, expect
-from src.utils.assertions import is_selected
+
 from src.page_objects.base_page import BasePage
-from src.models.auth.user_model import UserModel
+from src.utils.assertions import is_selected
 
 
 class CustomersPage(BasePage):
-    def __init__(self, page: Page, user: UserModel):
+    def __init__(self, page: Page):
         super().__init__(page)
-        self.user = user
         self.tabs = {
             "active": page.get_by_role("tab", name=re.compile("Active customers.*")),
             "new": page.get_by_role("tab", name=re.compile("New customers.*")),
             "past": page.get_by_role("tab", name=re.compile("Past customers.*")),
         }
-        self.active_customers_table_headers = [
-            page.get_by_role("columnheader", name=re.compile("Company.*")),
-            page.get_by_role("columnheader", name=re.compile("Status.*")),
-            page.get_by_role("columnheader", name=re.compile("Employee limit.*")),
-            page.get_by_role("columnheader", name=re.compile("Actions.*")),
-        ]
-        self.copy_invitation_link_button = page.get_by_label(
-            "Copy a unique link to invite"
-        )
-        if not user.is_reseller:
-            self.active_customers_table_headers.append(
-                page.get_by_role("columnheader", name=re.compile("Reseller.*")),
-            )
+        self.active_customers_table_headers = page.get_by_role("columnheader")
+        self.copy_invitation_link_button = page.get_by_label("Copy a unique link to invite")
 
     @allure.step("CustomersPage: validate everything is visible")
     def validate_elements_visible(self):
@@ -41,7 +30,7 @@ class CustomersPage(BasePage):
 
     @allure.step("CustomersPage: validate all active customers headers are visible")
     def validate_active_customers_headers_are_visible(self):
-        for header in self.active_customers_table_headers:
+        for header in self.active_customers_table_headers.all():
             expect(header).to_be_visible()
 
     @allure.step
