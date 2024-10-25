ae19f744361385995f3495ab40032e417bdff7bb
pham duc <pham.duc@aironworks.com>
2024-10-25 06:11:15 +0000

    K6 performance

---
+++ tests/tests_perf_k6/confirm_education_content_perf.js
@@ -0,0 +1,49 @@
+import http from 'k6/http';
+// import { open } from 'k6/experimental/fs';
+import papaparse from 'https://jslib.k6.io/papaparse/5.1.1/index.js';
+import { SharedArray } from 'k6/data';
+import { check, sleep } from 'k6';
+
+const BASE_URL = 'https://staging.app.aironworks.com';
+const data = new SharedArray('users', function () {
+  // here you can open files, and then do additional processing or generate the array with data dynamically
+  const f = JSON.parse(open('./perf_education_campaign.json')).users;
+  return f; // f must be an array[]
+});
+
+export let options = {
+    vus: 10,
+    duration: '20s',
+    iterations: 100,
+};
+
+export default function(){
+    //Get data from data set
+    let currentIndex = __ITER % data.length;
+    let row = data[currentIndex];
+    console.log("Data:"+typeof(row));
+    let assignmentId = row.id;
+    let url = BASE_URL + '/api/education/assignment/'+ assignmentId;
+    let params = {
+        'email': row.email, 
+        'token': row.token,
+    };
+    let res = http.get(url, params=params);
+    //Check if response is not 200
+    check(res, {
+        'is status 200': (r) => r.status === 200,
+    });
+    let url2 = BASE_URL + '/api/education/assignment/'+ assignmentId +'/submit/';
+    let params2 = {
+        'email': row['email'], 
+        'token': row['token'],    
+    };
+    let res2 = http.post(url2, params=params2);
+    //Check if response is not 200
+    check(res2, {
+        'is status 200': (r) => r.status === 200,
+    });
+}
+
+
+
