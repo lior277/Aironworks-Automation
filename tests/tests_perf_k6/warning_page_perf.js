56fdb887d9bdf9b76c3757868f8805f8747b97d5
Bar Perach <bar@aironworks.com>
2024-11-06 06:50:59 +0000

    warning page doesn't work

--- tests/tests_perf_k6/warning_page_perf.js
+++ tests/tests_perf_k6/warning_page_perf.js
@@ -1,4 +1,5 @@
 import http from 'k6/http';
+import exec from 'k6/execution';
 // import { open } from 'k6/experimental/fs';
 import papaparse from 'https://jslib.k6.io/papaparse/5.1.1/index.js';
 import { SharedArray } from 'k6/data';
@@ -12,26 +13,37 @@ const data = new SharedArray('cases', function () {
 });
 
 export let options = {
-    vus: 10,
-    duration: '20s',
-    iterations: 100,
+  scenarios: {
+    warning_page: {
+      executor: 'per-vu-iterations',
+      vus: 100,
+      iterations: 30,
+      maxDuration: '30s',
+    }
+  }
 };
 
 export default function(){
-    let tags = { testid: 'k8s' };
+    let tags = { testid: 'warning_page' };
     //Get data from data set
-    let currentIndex = __ITER % data.length;
+    let currentIndex = exec.scenario.iterationInTest % data.length;
+    console.log("Current Index:"+currentIndex);
     let row = data[currentIndex];
     console.log("Data:"+typeof(row));
     let url = row.attack_url;
 
     let payload = JSON.stringify({'url': url });
     let params = {
-    headers: {
-      'Content-Type': 'application/json',
-    },
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      tags: tags
     };
-    let res = http.post(BASE_URL + '/api/public/verify_url_click', payload, params,{tags: tags});
+    let res = http.post(BASE_URL + '/api/public/verify_url_click', payload, params);
+
+    console.log("Response code:"+res.status);
+    console.log("Response:"+res.body);
+
     //Check if response is not 200
     check(res, {
         'is status 200': (r) => r.status === 200,
