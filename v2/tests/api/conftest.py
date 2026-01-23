6321e2a9826d41be32fb7e54c52cde87da3833aa
lior <lior277@gmail.com>
2026-01-23 11:49:13 +0200

    fix test_upload_groups

--- v2/tests/api/conftest.py
+++ v2/tests/api/conftest.py
@@ -1,8 +1,11 @@
+"""API-specific fixtures."""
+
 import pytest
 
 from v2.src.pages.api_pages.campaigns_api import CampaignsApi
 
 
 @pytest.fixture
-def campaigns_api(api_session):
+def campaigns_api(api_session) -> CampaignsApi:
+    """Campaigns API helper."""
     return CampaignsApi(api_session)
